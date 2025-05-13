7c69848754d3f9a5889efbf44552674bcd730db8
smmane <ymane.sm@gmail.com>
2025-05-13 16:21:03 +0000

    feat(resumes): ajouter le component resumes

---
+++ src/components/resumes/Resumes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './resumes.css';
+
+
+const Resumes = () =>{
+          return (
+                    <div>Resumes</div>
+          )
+}
+
+export default Resumes
