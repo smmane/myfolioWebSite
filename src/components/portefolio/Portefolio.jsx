37931d695e36e0173c94e436fbd509e486d92cc3
smmane <ymane.sm@gmail.com>
2025-05-13 16:26:03 +0000

    feat(portefolio) : ajouter le component Portefolio

---
+++ src/components/portefolio/Portefolio.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './portefolio.css';
+
+const Portefolio = () =>{
+          return(
+                    <div>Portfolio</div>
+          )
+}
+
+export default Portefolio
