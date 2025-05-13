bfbf6d95fa0dd5724b13f2b2709534dd0fc31b41
smmane <ymane.sm@gmail.com>
2025-05-13 16:55:05 +0000

    feat(app): ajouter le sidebar et des differente compoenent dans le App principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import { useState } from 'react'
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Portefolio from './components/portefolio/Portefolio'
+import Resumes from './components/resumes/Resumes'
+import Services from './components/services/Services'
+import Sidebar from './components/sidebar/Sidebar'
+import Contact from './components/contact/Contact'
+import Pricing from './components/pricing/Pricing'
+import Blog from './components/blog/Blog'
+import Testimonial from './components/testimonial/Testimonial'
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    SEYDOU MANE
+      <Sidebar />
+      <main className='main'>
+        <Home />
+        <About />
+        <Services />
+        <Resumes />
+        <Portefolio />
+        <Pricing />
+        <Testimonial />
+        <Blog />
+        <Contact />
+      </main>
     </>
   )
 }
